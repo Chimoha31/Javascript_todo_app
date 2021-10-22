5be635c8684344b56c932459c0e38736becbe8ad
Chimoha31 <mokochii1108@gmail.com>
2021-10-21 17:01:36 -0700

    complete todo app for JS.

--- js/main.js
+++ js/main.js
@@ -4,14 +4,24 @@ const onClickAdd = () => {
   const inputText = document.getElementById("add_text").value;
   document.getElementById('add_text').value = '';
 
-//div生成・DOM作り
+  createIncompleteList(inputText);
+};
+
+//未完了リストから指定の要素を削除
+const deleteFromIncompleteList = (target) => {
+  document.getElementById('incomplete_list').removeChild(target);
+}
+
+//未完了リストに追加する関数
+const createIncompleteList = (text) => {
+
+  //div生成・DOM作り
 const div = document.createElement('div');
 div.className = "list_row";
-// console.log(div);
 
 //liタグ生成
 const li = document.createElement('li');
-li.textContent = inputText;
+li.textContent = text;
 // console.log(li);
 
 //button(完了)タグ生成
@@ -19,11 +29,9 @@ const completeButton = document.createElement('button');
 completeButton.textContent = '完了';
 
 completeButton.addEventListener('click', () => {
-  deleteFromIncompleteList(completeButton.parentNode);
-
   const addTarget = completeButton.parentNode;
   const text = addTarget.firstElementChild.innerText;
-
+  
   addTarget.textContent = null;
   const li = document.createElement('li');
   li.innerText = text;
@@ -31,6 +39,15 @@ completeButton.addEventListener('click', () => {
   // button(戻す)生成
   const backButton = document.createElement('button');
   backButton.innerText = '戻す';
+  
+  backButton.addEventListener('click', () => {
+    const deleteTarget = backButton.parentNode;
+    document.getElementById('complete_list').removeChild(deleteTarget);
+
+    //テキストを取得
+    const text = backButton.parentNode.firstElementChild.innerText;
+    createIncompleteList(text)
+  });
 
   addTarget.appendChild(li);
   addTarget.appendChild(backButton);
@@ -56,10 +73,6 @@ div.appendChild(deleteButton);
 
 //未完了リストにdivを付ける
 document.getElementById('incomplete_list').appendChild(div);
-};
-
-const deleteFromIncompleteList = (target) => {
-  document.getElementById('incomplete_list').removeChild(target);
 }
 
 document.getElementById('add_button').addEventListener('click', () => onClickAdd());
